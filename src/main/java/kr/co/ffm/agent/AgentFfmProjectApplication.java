798cf4cafbb96ec76524f5ddc8a3018db7c969f4
WangiC <ChoWangi>
2022-05-14 16:13:07 +0900

    Initial commit

---
+++ src/main/java/kr/co/ffm/agent/AgentFfmProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ffm.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentFfmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentFfmProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/ffm/agent/AgentFfmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ffm.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentFfmProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
